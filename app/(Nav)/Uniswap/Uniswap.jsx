2f19be7f1eb7da0f2402160e42e8092c226bec38
QinJiangBan <421144305@qq.com>
2023-07-08 15:09:35 +0800

    RPC

--- app/(Nav)/Uniswap/Uniswap.jsx
+++ app/(Nav)/Uniswap/Uniswap.jsx
@@ -1,62 +1,52 @@
-'use client'
-import './Uniswap.css'
-import { SwapWidget, darkTheme  } from '@uniswap/widgets'
-import { provider } from '../../_providers/Thirdweb'
-import { useTranslation } from 'react-i18next';
-import { useTheme } from 'next-themes';
+"use client";
+import "./Uniswap.css";
+import { SwapWidget, darkTheme } from "@uniswap/widgets";
 
-export default  function Uniswap() {
-const { t } = useTranslation();
-const { resolvedTheme } = useTheme();
-  return(<div className="Uniswap1">
+import { useTranslation } from "react-i18next";
+import { useTheme } from "next-themes";
 
-<div className="Uniswap">
+export default function Uniswap() {
+  const { t } = useTranslation();
+  const { resolvedTheme } = useTheme();
+  return (
+    <div className="Uniswap1">
+      <div className="Uniswap">
+        <SwapWidget
+          convenienceFee={1} // 指定便利费为 1 个基点
+          convenienceFeeRecipient="0xcd284038f2E68c6A43b04695f84377f38686eE56" //使用该地址作为便利费收款方
+          jsonRpcUrlMap={jsonRpcUrlMap} //Api节点
+          width={"100%"}
+          locale={t("zh-CN")} //指定小组件界面的显式区域设置为中文
+          theme={resolvedTheme === "dark" ? darkTheme : customTheme} //覆盖样式属性
+          brandedFooter={false} //启用小部件底部的“由 Uniswap 提供支持”页脚。
+          hideConnectionUI={true} //隐藏小部件的内置钱包连接UI,'true'
 
-    <SwapWidget 
-      width={"100%"}
-      theme={resolvedTheme === 'dark' ? darkTheme : customTheme}//覆盖样式属性
-      locale={t('zh-CN')} //指定小组件界面的显式区域设置为中文
-      hideConnectionUI={true} //隐藏小部件的内置钱包连接UI,'true'
-      brandedFooter={false} //启用小部件底部的“由 Uniswap 提供支持”页脚。
-      jsonRpcUrlMap={jsonRpcUrlMap} //Api节点
-      provider={provider} //web3 提供程序
-
-/*       provider={provider}
-      account={account}  */
-      /* defaultChainId={1} //指定要提示用户将其钱包连接到哪个链ID */
-      //tokenList={UNISWAP_TOKEN_LIST}          //令牌列表
-      defaultInputTokenAddress={NATIVE}   //本机令牌的特殊地址
-      defaultInputAmount={1}          //默认输入设置为 1 代币
-      defaultOutputTokenAddress={USDT}         //默认输出令牌设置为 USDT
-      convenienceFee={1} // 指定便利费为 1 个基点
-      convenienceFeeRecipient="0xcd284038f2E68c6A43b04695f84377f38686eE56"//使用该地址作为便利费收款方
-
-
-     
-    />
-
-
-  </div>
-
-  
-  </div>
+          //defaultChainId={1} //指定要提示用户将其钱包连接到哪个链ID
+          //tokenList={UNISWAP_TOKEN_LIST}          //令牌列表
+          //defaultInputTokenAddress={NATIVE}   //本机令牌的特殊地址
+          //defaultInputAmount={1}          //默认输入设置为 1 代币
+          //defaultOutputTokenAddress={USDT}  //默认输出令牌设置为 USDT
+        />
+      </div>
+    </div>
   );
 }
-/* const light ={
-} */
+
 const customTheme = {
-  interactive: '#ea7411',
-  accent: '#ea7411',
+  interactive: "#ea7411",
+  accent: "#ea7411",
   borderRadius: 0,
 };
-const jsonRpcUrlMap = { 
-  1:['https://eth-mainnet.g.alchemy.com/v2/mlFEUUHXOMfXKOL0zV12xXYfb8LUjnMf'],
-  42161:['https://arb-mainnet.g.alchemy.com/v2/4ZFkWe3RAFIPggBGs_QF22l_CkPQIwv0'],
-  10:['https://opt-mainnet.g.alchemy.com/v2/mWJ8qFvl-bijKQGL85sSLK54Audbbt_o'],
-  137:['https://polygon-mainnet.g.alchemy.com/v2/KYaEAzPvehDQOFBO5tAjIX2_FRwoH1oD']
-  }
-const NATIVE = 'NATIVE' 
-const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+const jsonRpcUrlMap = {
+  1: ["https://eth-mainnet.g.alchemy.com/v2/mlFEUUHXOMfXKOL0zV12xXYfb8LUjnMf"],
+  42161: [
+    "https://arb-mainnet.g.alchemy.com/v2/4ZFkWe3RAFIPggBGs_QF22l_CkPQIwv0",
+  ],
+  10: ["https://opt-mainnet.g.alchemy.com/v2/mWJ8qFvl-bijKQGL85sSLK54Audbbt_o"],
+  137: [
+    "https://polygon-mainnet.g.alchemy.com/v2/KYaEAzPvehDQOFBO5tAjIX2_FRwoH1oD",
+  ],
+};
+//const NATIVE = 'NATIVE'
+//const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
 //const QLC ='0x6B175474E89094C44Da98b954EedeAC495271d0F'
-
-
--- app/_providers/Thirdweb.js
+++ app/_providers/Thirdweb.js
@@ -19,38 +19,51 @@ import {
 export default function Thirdweb({ children }) {
   const { t } = useTranslation();
 
-const metamaskConfig = metamaskWallet();
-metamaskConfig.meta.name = t('小狐狸钱包'); // 更改名称
-metamaskConfig.meta.iconURL = "/images/WEB3/metamask-fox.svg"; // 更改图标
-metamaskConfig.meta.urls = {
-  android: "https://metamask.app.link/bxwkE8oF99",
-  ios: "https://metamask.app.link/skAH3BaF99",
-  chrome: "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn",
-};
-// 覆盖连接 UI
-//metamaskConfig.connectUI = MetamaskConnectUI;
-// 自定义选择用户界面
-//metamaskConfig.selectUI = MetamaskSelectUI;
+  const metamaskConfig = metamaskWallet();
+  metamaskConfig.meta.name = t('小狐狸钱包'); // 更改名称
+  metamaskConfig.meta.iconURL = "/images/WEB3/metamask-fox.svg"; // 更改图标
+  metamaskConfig.meta.urls = {
+    android: "https://metamask.app.link/bxwkE8oF99",
+    ios: "https://metamask.app.link/skAH3BaF99",
+    chrome: "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn",
+  };
+  // 覆盖连接 UI
+  //metamaskConfig.connectUI = MetamaskConnectUI;
+  // 自定义选择用户界面
+  //metamaskConfig.selectUI = MetamaskSelectUI;
 
-const coinbaseConfig = coinbaseWallet();
-coinbaseConfig.meta.name = t('币库钱包'); 
+  const coinbaseConfig = coinbaseWallet();
+  coinbaseConfig.meta.name = t('币库钱包');
 
-const projectId = process.env.PROJECT_ID;
-const walletConnectConfig = walletConnect({
-  projectId: projectId,
-});
-walletConnectConfig.meta.name = t('钱包连接'); 
-walletConnectConfig.meta.iconURL = "/images/APPLOGO/wallet_connect.png"; 
+  const projectId = process.env.PROJECT_ID;
+  const walletConnectConfig = walletConnect({
+    projectId: projectId,
+  });
+  walletConnectConfig.meta.name = t('钱包连接');
+  walletConnectConfig.meta.iconURL = "/images/APPLOGO/wallet_connect.png";
 
   return (
 
 
     <ThirdwebProvider
       autoConnect={false}
-      activeChain="ethereum"
+      activeChain={{
+        ...Ethereum,
+        rpc: ["https://eth-mainnet.g.alchemy.com/v2/mlFEUUHXOMfXKOL0zV12xXYfb8LUjnMf"],
+
+        ...Polygon,
+        rpc: ["https://polygon-mainnet.g.alchemy.com/v2/KYaEAzPvehDQOFBO5tAjIX2_FRwoH1oD"],
+
+        ...Arbitrum,
+        rpc: ["https://arb-mainnet.g.alchemy.com/v2/4ZFkWe3RAFIPggBGs_QF22l_CkPQIwv0"],
+
+        ...Optimism,
+        rpc: ["https://opt-mainnet.g.alchemy.com/v2/mWJ8qFvl-bijKQGL85sSLK54Audbbt_o"],
+      }}
+
       supportedChains={[Ethereum, Polygon, Arbitrum, Optimism, Binance]}
-      supportedWallets={[  
-        metamaskWallet(), 
+      supportedWallets={[
+        metamaskWallet(),
         coinbaseWallet(),
         walletConnect()
       ]}
