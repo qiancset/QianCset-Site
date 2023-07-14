3a5be3e8972c628ce4bfd9617e7656414294891e
Qin <421144305@qq.com>
2023-07-15 01:38:29 +0800

    更新 layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: '千赛特 Qiancset Web3',
-  description: 'Qiancset Web3,千赛特区块链社区服务平台,去中心化点对点交易,web3互动交流的加密世界,公共、开源、优质的区块链服务平台,官方网站: https://www.qiancset.com/, 千赛特去中心化服务平台,qiancsetweb3一个点对点交易swap,NFT,nft,加密货币的Dapps,使用react next js 的webdapp',
+  description: 'Qiancset Web3,千赛特区块链社区服务平台,去中心化点对点交易,web3互动交流的加密世界,官方网站:www.qiancset.com, 千赛特去中心化服务平台,qiancsetweb3一个点对点交易swap,NFT,nft,加密货币的Dapps,使用react-nextjs的webdapp',
 
   /*   icons: {
       icon: '/favicon.ico',
