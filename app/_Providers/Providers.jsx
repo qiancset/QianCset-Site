fc35a5166e958af3c12098bfad3ce2190be6b046
QinJiangBan <421144305@qq.com>
2023-07-16 15:55:34 +0800

    更新 Providers.jsx

--- app/_Providers/Providers.jsx
+++ app/_Providers/Providers.jsx
@@ -2,11 +2,11 @@
 import AntdContainer from "./AntdContainer";
 import Themes from "./Themes";
 import Web3ModalP from './Web3ModalP';
-import { Analytics } from '@vercel/analytics/react';
+
 export default function Providers({ children }) {
   return (
     <>
-    <Analytics>
+  
       <Themes>
         <Web3ModalP>
           <AntdContainer>
@@ -14,7 +14,7 @@ export default function Providers({ children }) {
             </AntdContainer>
         </Web3ModalP>
       </Themes>
-      </Analytics>
+      
     </>
   );
 }
