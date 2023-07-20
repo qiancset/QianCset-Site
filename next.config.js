2fb7cfc9d1d075ffb50b42db683fef876e5c5278
QinJiangBan <421144305@qq.com>
2023-07-20 16:34:16 +0800

    @vercel/edge-config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        allowMiddlewareResponseBody: true,
-      },
-    async rewrites() {
-        if (process.env.VERCEL_ENV === 'production') {
-          const { middleware } = await import('./middleware');
-          return [
-            {
-              source: '/welcome',
-              destination: '/api/middleware',
-            },
-          ];
-        } else {
-          return [];
-        }
-      },
+
 
 }
 
