95868fdc7326707e353c4a3bbe59b2b3436bf87a
QinJiangBan <421144305@qq.com>
2023-09-03 18:03:40 +0800

    更新 Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,150 +1,137 @@
 /* eslint-disable @next/next/no-img-element */
-'use client'
-import './Footer.css'
-import Image from 'next/image';
-import Link from "next/link"
-import { useTranslation } from 'react-i18next';
-
+"use client";
+import "./Footer.css";
+import Image from "next/image";
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
+import QianCset from 'public/icon/QianCset.png'
 function Linkli({ children, href }) {
   return (
     <>
-      <li><Link href={href} target="_blank">{children}</Link></li>
+      <li>
+        <Link href={href} target="_blank">
+          {children}
+        </Link>
+      </li>
     </>
-  )
+  );
 }
 function SocialMedia({ href, className }) {
   return (
     <>
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href={href} target="_blank" rel="noopener noreferrer">
         <i className={className} />
       </a>
     </>
-  )
+  );
 }
 
 export default function Footer() {
   const { t } = useTranslation();
   return (
-    <footer className='footer'>
-<p>{t("关注我们的社交媒体")}</p>
+    <footer className="footer">
+      <p>{t("关注我们的社交媒体")}</p>
       <div className="social-media">
-        
-        <SocialMedia href='https://twitter.com/Qiancset' className='fab fa-twitter' />
-        <SocialMedia href='https://github.com/Qiancset' className='fab fa-github' />
-
+        <SocialMedia
+          href="https://twitter.com/Qiancset"
+          className="fab fa-twitter"
+        />
+        <SocialMedia
+          href="https://github.com/Qiancset"
+          className="fab fa-github"
+        />
       </div>
 
       <div className="列表">
-
-
-
-
-
-
         <div id="ff" className="content">
-
-
-
-
           <div className="link_boxes">
-
             <ul className="box1">
-              <li className="link_name"><h2>{t('产品')}</h2></li>
+              <li className="link_name">
+                <h2>{t("产品")}</h2>
+              </li>
 
-              <li><Link href="/" >{t('开发中')}...</Link></li>
-
-
+              <li>
+                <Link href="/">{t("开发中")}...</Link>
+              </li>
             </ul>
 
             <ul className="box2">
-              <li className="link_name"><h2>{t('介绍')}</h2></li>
-              <Linkli href='https://docs.qiancset.com/ru-men-zhi-nan' >
-                {t('入门指南')}
+              <li className="link_name">
+                <h2>{t("介绍")}</h2>
+              </li>
+              <Linkli href="https://docs.qiancset.com/ru-men-zhi-nan">
+                {t("入门指南")}
               </Linkli>
 
-              <li ><Link target="_blank" href="https://docs.qiancset.com/bai-pi-shu">
-                {t('白皮书')}
-              </Link></li>
-
+              <li>
+                <Link
+                  target="_blank"
+                  href="https://docs.qiancset.com/bai-pi-shu"
+                >
+                  {t("白皮书")}
+                </Link>
+              </li>
             </ul>
-
-
-
-
 
             <ul className="box3">
               <li className="link_name">
-                <h2>{t('服务')}</h2>
+                <h2>{t("服务")}</h2>
               </li>
 
               <Linkli href="https://docs.qiancset.com/Serve/ji-shu-wen-ti">
-                {t('技术问题')}
+                {t("技术问题")}
               </Linkli>
 
               <Linkli href="https://docs.qiancset.com/Serve/jia-ru-gong-xian">
-                {t('加入贡献')}
+                {t("加入贡献")}
               </Linkli>
 
               <Linkli href="https://docs.qiancset.com/category/he-zuo">
-                {t('合作')}
+                {t("合作")}
               </Linkli>
 
               <Linkli href="https://docs.qiancset.com/category/zan-zhu-shang">
-                {t('赞助商')}
+                {t("赞助商")}
               </Linkli>
-
             </ul>
 
             <ul className="box4">
               <li className="link_name">
-                <h2>{t('关于')}</h2>
+                <h2>{t("关于")}</h2>
               </li>
 
               <Linkli href="https://docs.qiancset.com/category/guan-yu-she-qu">
-                {t('关于社区')}
+                {t("关于社区")}
               </Linkli>
 
-
-
-
               <Linkli href="https://docs.qiancset.com/category/Legal_Terms/cookie-zheng-ce">
-                {t('Cookie政策')}
+                {t("Cookie政策")}
               </Linkli>
 
               <Linkli href="https://docs.qiancset.com/category/Legal_Terms/shi-yong-tiao-kuan">
-                {t('使用条款')}
+                {t("使用条款")}
               </Linkli>
               <Linkli href="https://docs.qiancset.com/category/Legal_Terms/yin-si-zheng-cen">
-                {t('隐私政策')}
+                {t("隐私政策")}
               </Linkli>
-
             </ul>
           </div>
-          <div className="content2">
-
-          </div>
-
-
+          <div className="content2"></div>
         </div>
-        <div className='logofooter'>
-          <Image src="/icon/QianCset.png" alt="QIANCSET" className='QIANCSET' height={132} width={400}/>
+        <div className="logofooter">
+          <Image
+            src={QianCset}
+            alt="QIANCSET"
+            className="QIANCSET"
+            height={132}
+            width={400}
+          />
         </div>
-
       </div>
-
 
       <div className="页眉">
-
-          <div className='版权'>
-            {t('版权所有')} © 2023 Qiancset 千赛特
-          </div>
-
+        <div className="版权">{t("版权所有")} © 2023 Qiancset 千赛特</div>
       </div>
-
     </footer>
   );
 }
