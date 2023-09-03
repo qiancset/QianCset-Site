8ecfb05335d3b987350c8c829d21488e16e2aea1
QinJiangBan <421144305@qq.com>
2023-09-03 18:27:57 +0800

    更新 Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,11 +4,9 @@ import "./Footer.css";
 import Image from "next/image";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
-<<<<<<< HEAD
-import QianCset from 'public/icon/QianCset.png'
-=======
+
 import QianCset from '../../public/icon/QianCset.png'
->>>>>>> main
+
 function Linkli({ children, href }) {
   return (
     <>
