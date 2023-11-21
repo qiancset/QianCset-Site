1b93157624ae27d30b1b1d757f2eec117edb91b9
qinjiangban <tanjiangban@gmail.com>
2023-11-21 22:10:32 +0800

    pagesBUG

--- app/(Apps)/(Navs)/(home)/layout.tsx
+++ app/(Apps)/(Navs)/(home)/layout.tsx
@@ -1,20 +1,25 @@
 'use client'
 import './Home.css'
 import { BsHouseCheck, BsHouses, BsHouseUp, BsPinMap } from 'react-icons/bs'
-import TabsButton from '@/Component/Button/TabsButton'
+
+import Link from 'next/link'
 
 export default function layout({ children }) {
     return (
         <>
             <div className='Home_Tabs'>
-                <TabsButton text={'整租'} icons={<BsHouseCheck />} fenlei={1} />
-                <TabsButton text={'合租'} icons={<BsHouses />} fenlei={2} />
-                <TabsButton text={'地图'} icons={<BsPinMap />} fenlei={3} />
-                <TabsButton text={'出租'} icons={<BsHouseUp />} fenlei={4} />
+            <Link href='/home/1' className='TabsButton'><BsHouseCheck />整租</Link>
+            <Link href='/home/2' className='TabsButton'><BsHouses />合租</Link>
+            <Link href='/home/3' className='TabsButton'><BsPinMap />地图</Link>
+            <Link href='/home/4' className='TabsButton'><BsHouseUp />出租</Link>
             </div>
+
+
+
             <h1>开发中...</h1>
             <h1>千赛特租房 Dapp</h1>
             {children}
         </>
     )
 }
+
