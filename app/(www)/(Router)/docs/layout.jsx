64d96261b487f278883caeaad5daa2596efba401
qinjiangban <tanjiangban@gmail.com>
2023-12-15 07:39:47 +0800

    docs css

--- app/(www)/(Router)/docs/layout.jsx
+++ app/(www)/(Router)/docs/layout.jsx
@@ -6,8 +6,7 @@ export default function AppsLayout({ children }) {
 
       <div className="www_page_container">
         <div className="www_page_left"> <Sidebar /></div>
-        <div className="www_page_center">{children}</div>
-        <div className="www_page_right"></div>
+        <div className="www_page_right">{children}</div>
       </div>
     </div>
   );
